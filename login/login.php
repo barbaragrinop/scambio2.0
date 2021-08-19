8a89ef75d5416b404c32d8726ed7f379133bd61c
Munir <munirarabi04@gmail.com>
2021-08-19 18:30:26 -0300

    validações

--- login/login.php
+++ login/login.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Scambio</title>
+	<title>Scambio | Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
