50b768e0ac9b1b2733430e0d18c7dac121586300
Barbara Hellen <barbarapereira123@hotmail.com>
2021-11-02 20:15:36 -0300

    salvando

---
+++ assets/js/cadastroPublicacao.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("#frmRecuperaCodigo"), 
+btnCad = document.querySelector("#btnCadastrar")
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+}
+
+btnCad.onclick = () => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", )
+}
+
