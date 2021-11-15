7d4eba7d4c503ce38a2ec5d22dadcea162d48c4f
LooseBleb <yago.tavares@etec.sp.gov.br>
2021-11-15 12:39:17 -0300

    ajax-home-funcionando

--- assets/js/ajaxhome.js
+++ assets/js/ajaxhome.js
@@ -9,38 +9,7 @@ $(document).ready( () => {
         success: function (response) {
             console.log(response);
             $('.return').html(response);
-        }
-        
+        }    
     })
-})
-$("#btnCadastrar").click( () => {
-    let filtro = $('input[search]').val();
-    $.ajax({
-        type: "post",
-        url: "PHP/SELECT_LIVROS_PUBLICADOS.php",
-        data: {
-            "search": filtro,
-        },
-        success: function (response) {
-            console.log(response);
-            $('.return').html(response);
-        }
-        
-    })
-})
+});
 
-$(".filtro").serialize( () => {
-    let filtro = $('#search').val();
-    $.ajax({
-        type: "post",
-        url: "PHP/SELECT_LIVROS_PUBLICADOS.php",
-        data: {
-            "search": filtro,
-        },
-        success: function (response) {
-            console.log(response);
-            $('.return').html(response);
-        }
-        
-    })
-})
