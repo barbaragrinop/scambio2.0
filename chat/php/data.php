ff6307297d8036f8212fed29fcfac10c478a31c5
Barbara Hellen <barbara.pereira@modalgr.com.br>
2021-11-24 18:06:43 -0300

    pra não perder

--- chat/php/data.php
+++ chat/php/data.php
@@ -1,6 +1,7 @@
 <?php
     $you = " ";
     while($row = $sql->fetch(PDO::FETCH_ASSOC)){ 
+                            // $sql->closeCursor();        
         $query2 = $pdo->prepare("SELECT * from db_scambio.messages where (incoming_msg_id = :unique_id
                                 or outgoing_msg_id = :unique_id) and (outgoing_msg_id = :outgoing 
                                 or incoming_msg_id = :outgoing) order by msg_id desc limit 1 "); //pegando a ultima mensagem
@@ -8,21 +9,24 @@
             ':outgoing' => $outgoing_id, 
             ':unique_id' => $row['cd_usuario']
         ));
-
-        $row2 = $query2->fetch(PDO::FETCH_ASSOC);
+        
+        
         if($query2->rowCount() > 0){
             $result = $row2['msg'];
         } else{
             $result = "";
         }
-
+        // $sql->closeCursor();     
+        
         //cortando a mensagem p caber na visualização
         (strlen($result) > 24) ? $msg = substr($result, 0, 24).'...' :  $msg = $result;
-
+        
         //adiocionando na frente do texto
         ($outgoing_id == ($row2['outgoing_msg_id'] ?? "")) ? $you = "Você: " : $you = "";
         // echo $outgoing_id . " - " . $row2['outgoing_msg_id'];
+        $row2 = $query2->fetch(PDO::FETCH_ASSOC);
 
+        
         //checando se o usuário está online ou offline
         ($row['nm_status'] == "Offline") ? $offline = "offline" : $offline = "";   
 
--- chat/php/users.php
+++ chat/php/users.php
@@ -6,25 +6,22 @@ $outgoing_id = $_SESSION['id'];
 
 $output = " ";
 
-$sql = $pdo->prepare("SELECT DISTINCT * from db_scambio.tb_usuario
-                        left join db_scambio.messages on 
-                        tb_usuario.cd_usuario = messages.outgoing_msg_id 
-                        where not cd_usuario = :user
-                        group by cd_usuario
-                        order by datemessage desc
-");
+$sql = $pdo->prepare("CALL db_scambio.sp_ordenaConversasChat(:user)");
 $sql->execute(array(':user' => $outgoing_id));
+
 
 if($sql->rowCount() <= 0){ 
     $output = "Sem usuários para conversar.";
     // echo $output;
-    echo $sql->rowCount();
+    // echo $sql->rowCount();
 }
 
 if($sql->rowCount() > 0){
     include 'data.php';
+    $sql->closeCursor();        
 }
 
 echo $output;   
+
 
 ?>
