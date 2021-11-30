6f6ae55860c57e3895ba44869aabd622a5b3a385
Munir Arabi Marques <munirarabi04@gmail.com>
2021-11-30 14:28:19 -0300

    mudando css

--- perfil/PHP/select_info_user.php
+++ perfil/PHP/select_info_user.php
@@ -15,22 +15,22 @@ if ($query->rowCount() > 0) {
             <form id="formupdate">
                 <h1 style="padding-left: 1px; margin-top: -50px;">Biografia | Editar</h1>
                 <div class="bio-row">
-                    <p style="margin-top: 25px;"><span>Nome:</span><input type="text" class="form-control" name="newnome" id="newnome" style="width: 400px !important;" value ="' . $row['nm_usuario'] .'"></p>
+                    <p style="margin-top: 25px;"><span>Nome:</span><input type="text" class="form-control" name="newnome" id="newnome" style="width: 350px !important;" value ="' . $row['nm_usuario'] . '"></p>
                 </div>
                 <div class="bio-row">
-                    <p style="margin-top: 25px;"><span>E-mail:</span><input type="text" class="form-control" id="newemail" name="newemail" style="width: 400px !important;" value ="' . $row['nm_email'] .'"></p>
+                    <p style="margin-top: 25px;"><span>E-mail:</span><input type="text" class="form-control" id="newemail" name="newemail" style="width: 350px !important;" value ="' . $row['nm_email'] . '"></p>
                 </div>
                 <div class="bio-row">
-                    <p style="margin-top: 35px;"><span>Nascimento:</span><input type="text" class="form-control" name="newborn" id="newborn" style="width: 400px !important;" value ="' . $row['dt_nascimento'] .'"></p>
+                    <p style="margin-top: 35px;"><span>Nascimento:</span><input type="text" class="form-control" name="newborn" id="newborn" style="width: 350px !important;" value ="' . $row['dt_nascimento'] . '"></p>
                 </div>
                 <div class="bio-row">
-                    <p style="margin-top: 35px;"><span>CEP: </span><input type="text"class="form-control" name="newaddres" id="newaddres" style="width: 400px !important;"></p>
+                    <p style="margin-top: 35px;"><span>CEP: </span><input type="text"class="form-control" name="newaddres" id="newaddres" style="width: 350px !important;"></p>
                 </div>
                 <div class="up-img" style="padding-top: 20px; padding-left: 17px; display: flex; flex-direction: row; margin-top: 260px;">
                     <span style="font-size: 15px;">Imagem de Perfil</span>
                     <input type="file" style="margin-left: 20px;" name="newimage" id="newimage">
                 </div>
-                <div class="btn-confirm-edit" style="margin-left: 700px; border: none;">
+                <div class="btn-confirm-edit" style="margin-left: 600px; border: none;">
                     <button id="updateinfous" onclick="update()" style="border: none; border-radius: 7px; height: 35px; width: 90px; background-color: #AC7E55; color: white; margin-left: 90px;">Confirmar</button>
                 </div>
             </form>
@@ -40,4 +40,3 @@ if ($query->rowCount() > 0) {
 
     echo $out;
 }
-?>
--- perfil/perfil2.php
+++ perfil/perfil2.php
@@ -287,16 +287,17 @@ if ($sql->rowCount() >= 1) {
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                            <div class="modal-body">
-                                Tem certeza que deseja excluir essa postagem ?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                            <div class="modal-body" style="display: flex;">
+                                <p style="margin-top: 10px; margin-right: 13px;">Tem certeza que deseja excluir essa postagem ?</p>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal" style="margin-bottom: 5px; margin-right: 10px;">Fechar</button>
                                 <form method="POST">
                                     <input type="hidden" id="idlb" name="cdlb">
                                     <input id="btn-del" type="submit" value="Excluir" class="btn btn-danger">
                                 </form>
                             </div>
+                            <!-- <div class="modal-footer" style="display: flex;">
+
+                            </div> -->
                         </div>
                     </div>
                 </div>
@@ -315,12 +316,11 @@ if ($sql->rowCount() >= 1) {
                             <form method="POST">
                                 <div class="modal-body" style="display: inline-block; flex-direction: column;">
                                     <div class="form-group">
-                                        <img src="" id="imgedit" width="200px;">
+                                        <img src="" id="imgedit" width="200px;" height="330px">
                                     </div>
 
                                     <div style="display: flex; flex-direction: column; margin-left: 30px;">
-
-                                        <div class="form-group" style=" margin-left: 200px; margin-top: -330px;">
+                                        <div class="form-group" style=" margin-left: 200px; margin-top: -350px;">
                                             <label for="recipient-name" class="col-form-label">Livro:</label>
                                             <input type="text" class="form-control" name="livro" id="lbedit" style="width: 300px !important;">
                                         </div>
